fa2538627fc807de55b524cbeacbc298a2df5e73
Adrien <adrien.schmidt7@gmail.com>
2022-10-29 10:45:09 +0200

    Ajout du prix des produits

--- class/Produit.js
+++ class/Produit.js
@@ -4,6 +4,8 @@ class Produit {
     prixVente;
     constructor(nom, coutFabrication, prixVente) {
         this.nom = nom;
+        this.coutFabrication = coutFabrication;
+        this.prixVente = prixVente;
         console.log(`${this.nom} est un produit (${this.coutFabrication}/${this.prixVente})`);
     }
     
