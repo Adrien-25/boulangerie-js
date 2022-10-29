43c2dda5ea4e9a6e005a878956fc96cd949e35e2
Adrien <adrien.schmidt7@gmail.com>
2022-10-29 11:35:53 +0200

    Affichage des prix avec deux décimales

--- class/Patisserie.js
+++ class/Patisserie.js
@@ -3,7 +3,7 @@ class Patisserie extends Produit {
     constructor(nom,coutFabrication,prixVente, auBeurre = false) {
         super(nom,coutFabrication,prixVente);
         this.auBeurre = auBeurre;
-        console.log(`${this.nom} est un produit (${this.coutFabrication}/${this.prixVente})`);
+        console.log(`${this.nom} est un produit (${this.coutFabrication.toFixed(2)}/${this.prixVente.toFixed(2)})`);
         console.log(`${this.nom} est précisément une pâtisserie ${this.auBeurre ? "au beurre" : ''}`);
     }
 }
--- class/Produit.js
+++ class/Produit.js
@@ -7,7 +7,7 @@ class Produit {
         this.coutFabrication = coutFabrication;
         this.prixVente = prixVente;
         if (this.nom != "charlotte"){   
-            console.log(`${this.nom} est un produit (${this.coutFabrication}/${this.prixVente})`);
+            console.log(`${this.nom} est un produit (${this.coutFabrication.toFixed(2)}/${this.prixVente.toFixed(2)})`);
         }
     }
     
